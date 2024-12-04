918fc05df5271969d0c93c409e2ee4937bcea167
Saurabh Nandwal <saurabh.nandwal@nimblework.com>
2024-12-04 15:08:14 +0530

    using streamBridge

--- src/main/java/org/nandwal/spring/order/ProducerApplication.java
+++ src/main/java/org/nandwal/spring/order/ProducerApplication.java
@@ -1,8 +1,6 @@
 package org.nandwal.spring.order;
 
-import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
--- src/main/java/org/nandwal/spring/order/controller/OrderController.java
+++ src/main/java/org/nandwal/spring/order/controller/OrderController.java
@@ -1,11 +1,10 @@
 package org.nandwal.spring.order.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.nandwal.spring.order.entity.Order;
-import org.nandwal.spring.order.kafka.OrderProducerConfig;
 import org.nandwal.spring.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.stream.function.StreamBridge;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -18,14 +17,8 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-    private final OrderProducerConfig orderProducerConfig;
-    private final ObjectMapper objectMapper;
-
     @Autowired
-    public OrderController(OrderProducerConfig orderProducerConfig, ObjectMapper objectMapper) {
-        this.orderProducerConfig = orderProducerConfig;
-        this.objectMapper = objectMapper;
-    }
+    private StreamBridge streamBridge;
 
     @GetMapping("/all")
     public List<Order> getAllOrders() {
@@ -35,8 +28,7 @@ public class OrderController {
     @PostMapping("/all")
     public void saveOrder(@RequestBody Order order) {
         try {
-            String orderJson = objectMapper.writeValueAsString(order);
-            orderProducerConfig.publish(orderJson);
+            streamBridge.send("sendOrderDetails-out-0",order);
             orderService.saveOrder(order);
             log.info("Order placed");
         } catch (Exception e) {
